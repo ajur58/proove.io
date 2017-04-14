a68b07edab8490ec87bae8894e98c06b994970f0
Adam Juras <hi@adamjuras.com>
2017-04-14 21:23:40 +0200

    Added navigation and sample pages

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,19 @@ router.get('/app', function (req, res, next) {
   res.render('app', { title: 'proove.io' })
 })
 
+/* GET home page. */
+router.get('/pricing', function (req, res, next) {
+  res.render('pricing', { title: 'proove.io' })
+})
+
+/* GET home page. */
+router.get('/blog', function (req, res, next) {
+  res.render('blog', { title: 'proove.io' })
+})
+
+/* GET home page. */
+router.get('/about', function (req, res, next) {
+  res.render('about', { title: 'proove.io' })
+})
+
 module.exports = router
